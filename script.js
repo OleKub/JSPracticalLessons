083b22ccad8efe4b120d9bb83edc38d587a4e310
OleksBr <bratytsia.o@gmail.com>
2022-12-05 00:17:35 +0200

    MovieQuestion

---
+++ script.js
@@ -0,0 +1,17 @@
+const numberOfFilms = prompt('Скільки фільмів ви вже подивились?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один із останніх переглянутих фільмів?', ''),
+      b = prompt('На скільки ви його оціните?', ''),
+      c = prompt('Один із останніх переглянутих фільмів?', ''),
+      d = prompt('На скільки ви його оціните?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
