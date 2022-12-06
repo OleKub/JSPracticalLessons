cedbac0494c16b045d7638f4d23f0686a788a09c
OleksBr <bratytsia.o@gmail.com>
2022-12-06 07:18:12 +0200

    testCycles3

--- scriptCycles.js
+++ scriptCycles.js
@@ -62,3 +62,64 @@ first: for (let i = 0; i < 4; i++) {
         }
     }
 }
+
+for (let i = 5; i < 11; i++) {
+    console.log(i);
+}
+
+for (let i = 20; i > 9; i--) {
+    if ( i == 13) break;
+    console.log(i);
+}
+
+for (let i = 2; i < 11; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+function fourthTask() {
+    let i = 2;
+
+    while (i <= 16) {
+        if (i % 2 === 0) {
+            i++;
+            continue;
+        } else {
+            console.log(i);
+        }
+        i++;
+    }
+}
+
+// const arrNumber = [];
+
+// for (let i = 5; i < 11; i++) {
+//     arrNumber[i - 5] = i;
+// }
+
+// console.log(arrNumber);
+// return arrNumber;
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//     result[i] = arr[i];
+// }
+
+// console.log(result);
+// return result;
+
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+// for (let i = 0; i < data.length; i++) {
+//     if (typeof(data[i]) === 'number') {
+//         data[i] = data[i] * 2;
+//     } else if (typeof(data[i])  === 'string') {
+//         data[i] = `${data[i]} - done`;
+//     }
+// }
+
+// console.log(data);
