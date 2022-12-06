85082de659cae5464f71b6fbc4ea763daf9d120f
OleksBr <bratytsia.o@gmail.com>
2022-12-06 06:14:22 +0200

    testCycles2

--- scriptCycles.js
+++ scriptCycles.js
@@ -1,22 +1,64 @@
 "use strict";
 
-let num = 50;
+//  let num = 50; 
 
-/* while (num <= 55) {
-    console.log(num);
-    num++;
-} */
+//  while (num <= 55) {
+//     console.log(num);
+//     num++;
+// } 
 
-/* do {
-    console.log(num);
-    num++;
-}
-while (num < 55) */
+//  do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55) 
 
-for (let i = 1; i < 8; i++) {
-    if (i === 6) {
-        break;
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         break;
+//         continue;
+//     }
+
+//     console.log(i);
+// } 
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+//     for (let i = 0; i < 3; i++) {
+//         console.log(i);
+//     }
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+let  result = '';
+const length = 7;
+
+for (let i =1; i < length; i++) {
+
+    for (let j = 0; j < i; j++) {
+        result += "*";
     }
+    
+    result += '\n';
+}
 
-    console.log(i);
+console.log(result);
+
+
+
+first: for (let i = 0; i < 4; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) continue first;
+            console.log(`Third level: ${k}`);
+        }
+    }
 }
