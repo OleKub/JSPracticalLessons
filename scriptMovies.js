d73da5abd5bb495a4667be4516674449b00c9dff
OleksBr <bratytsia.o@gmail.com>
2022-12-08 00:12:51 +0200

    testJSmovies2

--- scriptMovies.js
+++ scriptMovies.js
@@ -1,6 +1,6 @@
 const numberOfFilms = prompt('Скільки фільмів ви вже подивились?', '');
 
-const personalMovieDB = {
+ const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -8,10 +8,26 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один із останніх переглянутих фільмів?', ''),
-      b = prompt('На скільки ви його оціните?', ''),
-      c = prompt('Один із останніх переглянутих фільмів?', ''),
-      d = prompt('На скільки ви його оціните?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один із останніх переглянутих фільмів?', ''),
+          b = prompt('На скільки ви його оціните?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length <50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.lof('error');
+        i--;
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+    console.log("Перглянуто доволі мало фільмів");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Ви классний глядач");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Ви кіноман");
+} else {
+    console.log("Сталась прикра помилка");
+}
+
+console.log(personalMovieDB);
