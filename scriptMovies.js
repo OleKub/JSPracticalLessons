527fbde249e1e45f32997fb8a0dd8001bb1e73eb
SashaBr <bratytsia.o@gmail.com>
2022-12-09 16:52:41 +0200

    scriptMovieGenre

--- scriptMovies.js
+++ scriptMovies.js
@@ -1,6 +1,18 @@
-const numberOfFilms = prompt('Скільки фільмів ви вже подивились?', '');
+"use strict";
 
- const personalMovieDB = {
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
+    }
+}
+
+start();
+
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -8,26 +20,48 @@ const numberOfFilms = prompt('Скільки фільмів ви вже поди
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один із останніх переглянутих фільмів?', ''),
-          b = prompt('На скільки ви його оціните?', '');
-    if (a != null && b != null && a != '' && b != '' && a.length <50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.lof('error');
-        i--;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один із останніх переглянутих фільмів?', ''),
+              b = prompt('На скільки ви його оціните?', '');
+        if (a != null && b != null && a != '' && b != '' && a.length <50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.lof('error');
+            i--;
+        }
     }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log("Перглянуто доволі мало фільмів");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Ви классний глядач");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Ви кіноман");
-} else {
-    console.log("Сталась прикра помилка");
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Перглянуто доволі мало фільмів");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Ви классний глядач");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Ви кіноман");
+    } else {
+        console.log("Сталась прикра помилка");
+    }
 }
 
-console.log(personalMovieDB);
+detectPersonalLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] =  prompt(`Ваш улюблений жанр під номером ${i}`);
+    }
+}
+
+writeYourGenres();
--- scriptfunctionarrow.js
+++ scriptfunctionarrow.js
@@ -6,15 +6,46 @@
 
 // showFirstMessagev();
 
-const usdCurr = 28;
-const eurCurr = 32;
-let amount = 500;
+// const usdCurr = 28;
+// const eurCurr = 32;
+// let amount = 500;
 
-function convert(amount, curr) {
-    console.log(amount * curr);
+// function convert(amount, curr) {
+//     console.log(amount * curr);
+// }
+
+// convert(amount, usdCurr);
+// convert(amount, eurCurr);
+
+// const test = "12.4";
+// console.log(Math.round(test));
+
+function sayHello(name) {
+    return `Hi, ${name}!`;
 }
 
-convert(amount, usdCurr);
-convert(amount, eurCurr);
+sayHello('Alex');
 
-тест змін
+
+function returnNeighboringNumbers(num) {
+    return [num - 1, num, num + 1];
+}
+
+returnNeighboringNumbers(5);
+
+function getMathResult (num, times) {
+    if (typeof(times) !== 'number' || times <= 0) {
+        return num;
+    }
+
+    let str = '';
+
+    for (let i = 1; i <= times; i++) {
+        if (i == times) {
+            str+= `${num * i}`;
+        } else {
+            str += `${num * i}---`;
+        }
+    }
+    return str;
+}getMathResult(10, 5);
