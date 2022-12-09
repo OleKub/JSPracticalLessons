a96b29981b6d21bddc3a387e88ddd7c8c1502675
SashaBr <bratytsia.o@gmail.com>
2022-12-09 15:16:42 +0200

    traFunction2

---
+++ scriptfunctionarrow.js
@@ -0,0 +1,20 @@
+"use strict"
+
+// function showFirstMessagev() {
+//     console.log("Hellow World!");
+// }
+
+// showFirstMessagev();
+
+const usdCurr = 28;
+const eurCurr = 32;
+let amount = 500;
+
+function convert(amount, curr) {
+    console.log(amount * curr);
+}
+
+convert(amount, usdCurr);
+convert(amount, eurCurr);
+
+тест змін
