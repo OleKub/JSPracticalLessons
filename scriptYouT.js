7896d261c841ba8d8495111322fed62f4fb5d69c
SashaBr <bratytsia.o@gmail.com>
2022-12-15 00:20:26 +0200

    fitstLessYouT

---
+++ scriptYouT.js
@@ -0,0 +1,54 @@
+"use strics"
+
+// let a
+// console.log(a)
+// a = true 
+// console.log(a)
+
+// const myCity = {
+//     city: 'Kyiv',
+//     popular: true,
+//     country: 'Ukraine'
+// }
+// // console.log(myCity.city)
+// // console.log(myCity)
+// // myCity.city = 'Lviv'
+// // console.log(myCity)
+// myCity.region = 'Lviv region'
+// console.log(myCity)
+// delete myCity.city
+// console.log(myCity)
+
+// const myCity = {
+//     city: 'Kyiv'
+// }
+
+// myCity['popular'] = true
+// console.log(myCity)
+// const countryPropertyName = 'country'
+// myCity[countryPropertyName] = 'Ukraine'
+// console.log(myCity)
+
+
+
+// const myCity = {
+//     city: 'New York',
+//     info: {
+//         isPopular: true,
+//         country: 'USA'
+//     }
+// }
+// console.log(myCity.info['isPopular'])
+// delete myCity.info.isPopular
+// console.log(myCity)
+
+
+const uName = 'Oleksandr'
+const postsQty = 23
+
+const userProfile = {
+    uName,
+    postsQty,
+    hasSiqnedAgreement: false
+}
+console.log(userProfile)
--- scriptfunctionarrow.js
+++ scriptfunctionarrow.js
@@ -50,32 +50,33 @@
 //     return str;
 // }getMathResult(10, 5);
 
-function calculateVolumeAndArea(length) {
-    if (typeof (length) !== 'number' || length < 0 || !Number.isInteger(length)) {
-        return "При вычислении произошла ошибка";
-    }
+// function calculateVolumeAndArea(length) {
+//     if (typeof (length) !== 'number' || length < 0 || !Number.isInteger(length)) {
+//         return "При вычислении произошла ошибка";
+//     }
 
-    let volume = 0;
-         area = 0;
+//     let volume = 0;
+//          area = 0;
 
-    volume = length * length * length;
-    area = 6 * length * length;
+//     volume = length * length * length;
+//     area = 6 * length * length;
 
-    return `Объем куба: ${volume}, Площадь всей поверхности: ${area}`;
-}
+//     return `Объем куба: ${volume}, Площадь всей поверхности: ${area}`;
+// }
 
-calculateVolumeAndArea(5);
+// calculateVolumeAndArea(5);
 
-function getCoupeNumber(seatNumber) {
-    if (typeof (seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
-        return "Ошибка. ПРоверте правильность введенного номера места"
-    }
+// function getCoupeNumber(seatNumber) {
+//     if (typeof (seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
+//         return "Ошибка. ПРоверте правильность введенного номера места"
+//     }
 
-    if (seatNumber === 0 || seatNumber > 36) {
-        return "Таких мест в вагоне не существует";
-    }
+//     if (seatNumber === 0 || seatNumber > 36) {
+//         return "Таких мест в вагоне не существует";
+//     }
 
-    return Math.ceil(seatNumber / 4);
-}
+//     return Math.ceil(seatNumber / 4);
+// }
 
-getCoupeNumber(33);
+// getCoupeNumber(33);
+
