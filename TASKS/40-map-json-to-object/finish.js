0ae58c8fe4ea9a94dc159e5bd753b4dd6b0268f3
OleksandrBr <bratytsia.o@gmail.com>
2023-01-10 21:53:18 +0200

    task40

--- TASKS/40-map-json-to-object/finish.js
+++ TASKS/40-map-json-to-object/finish.js
@@ -15,3 +15,9 @@ const postsJSON = [
   '{"postId":6134,"commentsQuantity":2}',
   '{"postId":2351,"commentsQuantity":8}',
 ]
+
+const postsJS = postsJSON.map((postObject) => JSON.parse(postObject))
+
+console.log(postsJS)
+console.log(postsJS[1].postId)
+console.log(postsJS[postsJS.length - 1].commentsQuantity)
